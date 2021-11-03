5c826f82b06b871c637907d8229bed01762b0d39
Somil Khandelwal <somil@coffeebeans.io>
2021-11-03 18:07:25 +0530

    fix: fix login and signup.

--- login_page/urls.py
+++ login_page/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
   
     path('',views.home , name="home"),
-    path('signup',views.signup,name="signup"),
-    path('signin',views.signin,name="signin"),
+    path('/signup',views.signup,name="signup"),
+    path('/signin',views.signin,name="signin"),
     
 ]
