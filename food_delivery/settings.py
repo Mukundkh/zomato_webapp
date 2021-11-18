a15a18825466c96ce3f735322a9ef91869b0f8f4
Somil Khandelwal <somil@coffeebeans.io>
2021-11-18 18:13:48 +0530

    update

--- food_delivery/settings.py
+++ food_delivery/settings.py
@@ -167,4 +167,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'khandelwalmukund23@gmail.com'
-EMAIL_HOST_PASSWORD = 'bindassmukund'
+EMAIL_HOST_PASSWORD = 'xyz'
