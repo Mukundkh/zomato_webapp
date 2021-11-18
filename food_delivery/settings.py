f41c4f5e031bb967519701ddd98f08cb2a906865
Somil Khandelwal <somil@coffeebeans.io>
2021-11-18 18:11:12 +0530

    updated-feat

--- food_delivery/settings.py
+++ food_delivery/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+from django.core.mail import send_mail
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -149,8 +151,6 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
 ACCOUNT_ADAPTER = 'restaurant_dash.account_adapter.NoNewUsersAccountAdapter'
 
 LOGIN_REDIRECT_URL = 'dashboard'
@@ -161,3 +161,10 @@ LOGIN_REDIRECT_URL = 'index'
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'khandelwalmukund23@gmail.com'
+EMAIL_HOST_PASSWORD = 'bindassmukund'
